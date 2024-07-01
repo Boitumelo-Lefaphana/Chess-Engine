a815ae0de53a2729cbfe7988b3d87ff9484224e6
Boitumelo Lefaphana <boitumelolefaphana100@gmail.com>
2024-07-01 04:15:51 +0200

    first commit

---
+++ chess-webapp/src/main/java/com/lilo/chess_webapp/ChessWebApplication.java
@@ -0,0 +1,13 @@
+package com.lilo.chess_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessWebApplication.class, args);
+	}
+
+}
---
+++ chess-webapp/src/test/java/com/lilo/chess_webapp/ChessWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilo.chess_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
